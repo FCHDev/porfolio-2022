d4dcee32ce52115aee4e59d650d691e78f1757ba
FCHDev <acetige@gmail.com>
2022-11-10 13:00:32 +0100

    apple touch icon update + remove animated bg

--- src/components/SectionAccueil.js
+++ src/components/SectionAccueil.js
@@ -3,14 +3,14 @@ import Navigation from "./Navigation";
 import Speech from "./Speech";
 import PP from "./PP";
 
-import network from "../assets/vid/Network.mp4"
+// import network from "../assets/vid/Network.mp4"
 import SocialLogos from "./SocialLogos";
 
 
 const SectionAccueil = () => {
     return (
         <div className="section1" id="section1">
-            <video src={network} autoPlay muted loop className="video-bg"/>
+            {/*<video src={network} autoPlay muted loop className="video-bg"/>*/}
             <Navigation/>
             <PP/>
             <h1 className="fs-1 my-0 pt-3 pb-1">François Chevalier</h1>
